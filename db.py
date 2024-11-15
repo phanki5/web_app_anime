a9b368df99007ceec61d1a13fe961aa8ea5dc531
OemerFarukYildiz <167823684+OemerFarukYildiz@users.noreply.github.com>
2024-11-15 09:45:16 +0000

    Trennung APP.py und DB.PY + doku links

---
+++ db.py
@@ -0,0 +1,32 @@
+# db.py
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], 
+                         render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], 
+                          render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+    
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError("That username already exists. Please choose a different one.")
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], 
+                         render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], 
+                          render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
